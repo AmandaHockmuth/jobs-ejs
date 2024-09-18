1d2943669462eff397337af2fe98230b55004ed1
AmandaHockmuth <153574152+AmandaHockmuth@users.noreply.github.com>
2024-09-17 22:30:57 -0400

    entered ejs and partials, set up router and controllers, configured passport, set up authorization

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose
-    .connect(
-      url
-      //   , {
-      //   useNewUrlParser: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false,
-      //   useUnifiedTopology: true,
-      // }
-    )
-    .then(() => console.log("Connected to MongoDB"));
-  // .catch((err) => console.log("Error connecting to MongoDB:", err));
+  return mongoose.connect(url, {});
 };
 
 module.exports = connectDB;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 require("dotenv");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -34,16 +33,6 @@ UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
-
-UserSchema.methods.createJWT = function () {
-  return jwt.sign(
-    { userId: this._id, name: this.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_LIFETIME,
-    }
-  );
-};
 
 UserSchema.methods.checkPassword = async function (candidatePassword) {
   const isMatch = await bcrypt.compare(candidatePassword, this.password);
